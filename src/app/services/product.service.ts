80a01f0f5460ba6089e04cc13f50098ca6f0fec7
Robin9506 <robin.berg@hotmail.nl>
2022-12-05 23:42:49 +0100

    product style update

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,7 +9,7 @@ export class ProductService {
         new Product("Mario Kart", 49.99, "Racen!", "Nintendo"),
         new Product("Mario Kart 8", 79.99, "Racen maar hoger!", "Nintendo"),
         new Product("Mario Party", 39.99, "Feesten!", "Nintendo"),
-        new Product("Mario Party Allstars", 51.99, "Feesten again!", "Nintendo"),
+        new Product("Mario Party Allstarsss", 51.99, "Feesten again!", "Nintendo"),
         new Product("Xenoblade Chronicles", 40.00, "De toekomst!", "Monolith Soft"),
         new Product("Xenoblade Chronicles 2", 50.00, "De toekomst again!", "Monolith Soft"),
         new Product("Xenoblade Chronicles 3", 60.00, "De toekomst lost!", "Monolith Soft"),
