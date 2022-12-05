3997591a64d0913d0b8cc6572765f5c9e2fab4b8
Robin9506 <robin.berg@hotmail.nl>
2022-12-05 15:04:37 +0100

    product page styling

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,7 +7,12 @@ import { Product } from '../models/product.model';
 export class ProductService {
     private products: Product[] = [
         new Product("Mario Kart", 49.99, "Racen!", "Nintendo"),
-        new Product("Mario Party", 39.99, "Feesten!", "Nintendo")
+        new Product("Mario Kart 8", 79.99, "Racen maar hoger!", "Nintendo"),
+        new Product("Mario Party", 39.99, "Feesten!", "Nintendo"),
+        new Product("Mario Party Allstars", 51.99, "Feesten again!", "Nintendo"),
+        new Product("Xenoblade Chronicles", 40.00, "De toekomst!", "Monolith Soft"),
+        new Product("Xenoblade Chronicles 2", 50.00, "De toekomst again!", "Monolith Soft"),
+        new Product("Xenoblade Chronicles 3", 60.00, "De toekomst lost!", "Monolith Soft"),
     ];
 
   constructor() { }
